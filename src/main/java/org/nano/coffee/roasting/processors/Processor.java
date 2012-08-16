439ec121d4048e6b59ea851c7c22d5702c2b9c99
Clement Escoffier <clement.escoffier@gmail.com>
2012-08-16 09:39:52 +0200

    Changed processor interface

--- src/main/java/org/nano/coffee/roasting/processors/Processor.java
+++ src/main/java/org/nano/coffee/roasting/processors/Processor.java
@@ -1,9 +1,23 @@
 package org.nano.coffee.roasting.processors;
 
-import org.apache.maven.plugin.MojoExecutionException;
+import java.io.File;
+import java.util.Map;
 
 public interface Processor {
 
-    public void process() throws MojoExecutionException;
 
+    public void process(File input, Map<String, ?> options) throws ProcessorException;
+
+    public void tearDown();
+
+    public boolean accept(File file);
+
+    class ProcessorException extends Exception {
+        public ProcessorException(String message) {
+            super(message);
+        }
+        public ProcessorException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
 }
