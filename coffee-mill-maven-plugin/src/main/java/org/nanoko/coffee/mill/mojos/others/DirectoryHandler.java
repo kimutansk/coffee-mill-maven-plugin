addd42a4d7b509d8a8dcc915da31c3960b0aacc0
Clement Escoffier <clement.escoffier@gmail.com>
2013-02-13 17:59:04 +0100

    Fix copyright

--- coffee-mill-maven-plugin/src/main/java/org/nanoko/coffee/mill/mojos/others/DirectoryHandler.java
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffee/mill/mojos/others/DirectoryHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 OW2 Nanoko Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.nanoko.coffee.mill.mojos.others;
 
 import org.eclipse.jetty.server.handler.ResourceHandler;
@@ -5,14 +20,9 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import java.io.File;
 
 /**
- * Created with IntelliJ IDEA.
- * User: clement
- * Date: 16/08/12
- * Time: 12:57
- * To change this template use File | Settings | File Templates.
+ * Publishes a directory.
  */
 public class DirectoryHandler extends ResourceHandler {
-    private String prefix;
 
     public DirectoryHandler(File workDirectory) {
         setResourceBase(workDirectory.getAbsolutePath());
--- coffee-mill-maven-plugin/src/main/java/org/nanoko/coffee/mill/processors/CoffeeScriptCompilationProcessor.java
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffee/mill/processors/CoffeeScriptCompilationProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 OW2 Nanoko Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.nanoko.coffee.mill.processors;
 
 import org.apache.commons.io.FileUtils;
@@ -59,8 +74,7 @@ public class CoffeeScriptCompilationProcessor extends DefaultProcessor {
     private File getOutputJSFile(File input) {
         String jsFileName = input.getName().substring(0, input.getName().length() - ".coffee".length()) + ".js";
         String path = input.getParentFile().getAbsolutePath().substring(source.getAbsolutePath().length());
-        File theFile = new File(destination, path + "/" + jsFileName);
-        return theFile;
+        return new File(destination, path + "/" + jsFileName);
     }
 
     private void compile(File file) throws ProcessorException {
